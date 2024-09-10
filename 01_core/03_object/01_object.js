53d35989edf1443afe83db09b813a348723fbae0
kkandda <diobobo@naver.com>
2024-09-10 16:58:50 +0900

    함수를 부를 땐 ()까지 정확히 입력)

--- 01_core/03_object/01_object.js
+++ 01_core/03_object/01_object.js
@@ -9,17 +9,18 @@ object (객체)
 자바스크립트는 프로토타입 기반 객체지향 언어로 클래스 기반 언어와는 달리 다양한 객체 생성 방법을 지원한다.
 */
 
-// 객체 리터럴
+// 객체 리터럴(개발자가 생성한 의도된 값을 리터럴이라 한다.)
 let student ={
     name : "유관순",
     age : 16,
     getInfo : function(){
-        return `${this.name}은 ${this.age}세 입니다.`
+        console.log(`${this.name}은 ${this.age}세 입니다.`)
     }
 };
 
-console.log(typeof student)
-console.log(student)
+console.log(typeof student);
+console.log(student);
+console.log(student.getInfo());
 
 // Object 생성자 함수
 let student2 = new Object();
