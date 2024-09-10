17fbca73b174dd3d5c4670ee3dc5acc652d682f0
kkandda <diobobo@naver.com>
2024-09-10 17:18:38 +0900

    대괄호를 사용하여 키를 호출할 땐 어지간 하면  따옴표를 사용하자

--- 01_core/03_object/02_property.js
+++ 01_core/03_object/02_property.js
@@ -8,15 +8,17 @@ let student = {
 console.log(student);
 
 let obj = {
-    normal : "normal value",
-    '@ s p a c e @' : "space value",
+    normal : "normal value", // 프로퍼티는 키의 중복을 허락하지 않는다. 밑에서 확인해 보자.
+    '@ s p a c e @' : "space value", // 문자열 내의 띄어쓰기도 특수기호도 문자열일 뿐이다.
     "":"", // 빈문자열도 가능은 하다. 권장하지 않을 뿐..
-    0:1, // 문자로 바뀌고 나옴. 정수는 오름차순으로 정렬, 키 값은 기본적으로 문자열이다.
+    0:1, // 키가 문자로 바뀌어서 나옴. 그리고 정수라면 오름차순으로 정렬, 키는 기본적으로 문자열이다.
     var : "var", // 예약어도 가능은하다. 권장하지 않을 뿐..
-    normal : "normal" // 덮혔다. 키는 중복되지 않는다.
+    normal : "normal" // 키는 중복되지 않는다. 그래서 값이 덮혔다.
 };
 
-let key2 =1;
-obj[key2] = "test value";
+let key2 = 1;
+obj['11121'] = "test value"; // 대괄호를 사용하여 키를 호출했다.
+obj.normal="value";
+obj['normal']="yamyam";
 
 console.log(obj); // 변수 이름이 들어가는 것이 아니라, 변수 값이 들어간다. 프로퍼티 키를 동적으로 생성했다. 오름차순!
